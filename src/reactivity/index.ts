4df227f9256e110d7bc8c0653326f220c5ab6e5b
guoxingwang <wb-gxw02154864@alibaba-inc.com>
2022-04-24 21:10:00 +0800

    feat: 初始化仓库

---
+++ src/reactivity/index.ts
@@ -0,0 +1,3 @@
+export function add(a:any,b) {
+  return a+b
+}
---
+++ tests/index.spec.ts
@@ -0,0 +1,9 @@
+import { add } from "../src/reactivity"
+
+it('init',()=>{
+  expect(true).toBe(true)
+  expect(add(1,2)).toBe(3)
+  
+})
+
+
