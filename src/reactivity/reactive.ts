ad523d4912adff9e7b4c116dc4fb556d7cf22592
guoxingwang <wb-gxw02154864@alibaba-inc.com>
2022-04-28 10:08:09 +0800

    feat: add Proxy

--- src/reactivity/reactive.ts
+++ src/reactivity/reactive.ts
@@ -1,5 +1,17 @@
-export const reactive = (arg:any)=>{
-  return arg
+export const reactive = (raw:any)=>{
+
+  return new Proxy(raw,{
+    get(target,key){
+      const res = Reflect.get(target,key)
+      // 依赖收集
+      return res;
+    },
+    set(target,key,value){
+      const res = Reflect.set(target,key,value)
+      // 触发依赖
+      return res
+    }
+  })
 }
 
 export const effect = ()=>{
